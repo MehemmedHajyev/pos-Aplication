8f42fa024a65928544da46507cf2e0f5cb265326
MehemmedHajyev <hajyeffmehemmed8@gmail.com>
2024-01-08 11:41:22 +0400

    pos-app add first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button} from 'antd';
+
+
+function App() {
+  
+  return (
+    <>
+  <h1>hello</h1>
+  <Button type="primary">Primary</Button>
+    </>
+  )
+}
+
+export default App
